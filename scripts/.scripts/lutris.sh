69542fc35c982f8f6bb132a98bcf83e450385603
Łukasz Flisak <lu.dev00@gmail.com>
2024-10-11 09:42:54 +0200

    Update lutris installation script

--- scripts/.scripts/lutris.sh
+++ scripts/.scripts/lutris.sh
@@ -2,7 +2,7 @@
 
 # Install lutris + drivers for Intel and Radeon + wine dependencies
 
-sudo xbps-install void-repo{,-multilib}-nonfree
+sudo xbps-install void-repo-{nonfree,multilib,multilib-nonfree}
 sudo xbps-install -Su
 sudo xbps-install wine{,-devel}
 sudo xbps-install {mesa-vulkan-intel,mesa-vulkan-radeon,MesaLib-devel,alsa-lib-devel,alsa-plugins,libpulseaudio}{,-32bit} {vulkan-loader,v4l-utils,sqlite,SDL2,ocl-icd,libXcomposite,libva,alsa-lib,giflib,gnutls,gst-plugins-base1,libgamemode}{,-devel}{,-32bit} gamemode lutris

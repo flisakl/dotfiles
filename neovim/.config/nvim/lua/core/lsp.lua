7135f053de0c9033521b91eaa0f75bb58aaad701
Łukasz Flisak <lu.dev00@gmail.com>
2025-07-19 08:07:34 +0200

    Add completion plugin for neovim

--- neovim/.config/nvim/lua/core/lsp.lua
+++ neovim/.config/nvim/lua/core/lsp.lua
@@ -1,15 +1,3 @@
-vim.cmd[[set completeopt+=menuone,noselect,popup]]
-vim.lsp.config('*', {
-  on_attach = function(client, bufnr)
-    vim.lsp.completion.enable(true, client.id, bufnr, {
-      autotrigger = true,
-      convert = function(item)
-        return { abbr = item.label:gsub('%b()', '') }
-      end,
-    })
-  end,
-})
-
 vim.lsp.enable({
   'nixd', 'lua_ls', 'pylsp', 'vue_ls', 'ts_ls', 'tailwindcss'
 })

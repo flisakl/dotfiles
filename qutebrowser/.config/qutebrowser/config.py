46495383a966da2d208e493d6fd64701fba3e282
Łukasz Flisak <lu.dev00@gmail.com>
2025-08-15 10:10:21 +0200

    Add qutebrowser config

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,39 @@
+import os
+from urllib.request import urlopen
+from qutebrowser.config.configfiles import ConfigAPI  # noqa: F401
+from qutebrowser.config.config import ConfigContainer  # noqa: F401
+config: ConfigAPI = config  # noqa: F821 pylint: disable=E0602,C0103
+c: ConfigContainer = c  # noqa: F821 pylint: disable=E0602,C0103
+
+# Disable passthrough mode
+config.unbind('<Ctrl-v>')
+config.bind("<Ctrl-n>", "prompt-item-focus next", "prompt")
+config.bind("<Ctrl-p>", "prompt-item-focus prev", "prompt")
+config.bind("<Ctrl-r>", "config-source", "normal")
+
+c.content.autoplay = False
+c.downloads.position = "bottom"
+c.downloads.remove_finished = 10000
+c
+c.fonts.default_family = "CaskaydiaMono Nerd Font"
+c.fonts.default_size = "14px"
+c
+c.scrolling.bar = "always"
+c.url.searchengines = {
+    "DEFAULT": "https://duckduckgo.com/?q={}",
+    "dj": "https://docs.djangoproject.com/en/dev/search/?q={}"
+}
+
+
+# Load catppuccin theme
+config.load_autoconfig(False)
+
+if not os.path.exists(config.configdir / "theme.py"):
+    theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
+    with urlopen(theme) as themehtml:
+        with open(config.configdir / "theme.py", "a") as file:
+            file.writelines(themehtml.read().decode("utf-8"))
+
+if os.path.exists(config.configdir / "theme.py"):
+    import theme
+    theme.setup(c, 'mocha', True)
